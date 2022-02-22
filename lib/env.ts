72080aed56bc009c5e920ec7af7415f382c0c8bc
ivanjermakov <ivanjermakov1@gmail.com>
2022-02-22 08:46:16 +0100

    Github oauth

---
+++ lib/env.ts
@@ -0,0 +1,5 @@
+export const env = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_CLIENT_SECRET,
+    encryption_password: process.env.ENCRYPTION_PASSWORD,
+} as const
